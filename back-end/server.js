bd383adfa9dca5ca8e801bd0ddd14469e0a785f7
Grzegorz <gkopacki92@gmail.com>
2024-06-06 23:54:11 +0200

    .

--- back-end/server.js
+++ back-end/server.js
@@ -1,3 +1,112 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const fs = require("fs");
+const path = require("path");
+
+// Middleware do parsowania JSON
+app.use(express.json());
+
+// Serwowanie statycznych plików z folderu 'public'
+app.use(express.static("public"));
+
+// Endpoint dla ścieżki root
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
+});
+
+// Funkcja pomocnicza do czytania danych z pliku db.json
+const readData = (callback) => {
+	const dbPath = path.join(__dirname, "db.json");
+	fs.readFile(dbPath, "utf8", (err, data) => {
+		if (err) {
+			callback(err, null);
+		} else {
+			callback(null, JSON.parse(data));
+		}
+	});
+};
+
+// Funkcja pomocnicza do zapisu danych do pliku db.json
+const writeData = (data, callback) => {
+	const dbPath = path.join(__dirname, "db.json");
+	fs.writeFile(dbPath, JSON.stringify(data, null, 2), "utf8", (err) => {
+		callback(err);
+	});
+};
+
+// Endpointy dla różnych zasobów
+app.get("/women", (req, res) => {
+	readData((err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+		res.json(data.women || []);
+	});
+});
+
+app.get("/men", (req, res) => {
+	readData((err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+		res.json(data.men || []);
+	});
+});
+
+app.get("/children", (req, res) => {
+	readData((err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+		res.json(data.children || []);
+	});
+});
+
+app.get("/products", (req, res) => {
+	readData((err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+		res.json(data.products || []);
+	});
+});
+
+app.get("/favourites", (req, res) => {
+	readData((err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+		res.json(data.favourites || []);
+	});
+});
+
+app.post("/favourites", (req, res) => {
+	const { productId } = req.body;
+	if (!productId) {
+		return res.status(400).send("Product ID is required");
+	}
+
+	readData((err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+
+		const product = data.products.find((p) => p.id === productId);
+		if (!product) {
+			return res.status(404).send("Product not found");
+		}
+
+		data.favourites.push(product);
+		writeData(data, (err) => {
+			if (err) {
+				return res.status(500).send("Error writing to database file.");
+			}
+			res.status(201).send("Product added to favourites");
+		});
+	});
+});
+
 // Endpointy dla bestsellerów w różnych kategoriach
 app.get("/women/bestsellers", (req, res) => {
 	readData((err, data) => {
@@ -27,4 +136,9 @@ app.get("/children/bestsellers", (req, res) => {
 		const childrenBestsellers = data.children.bestsellers || [];
 		res.json(childrenBestsellers);
 	});
+});
+
+// Uruchomienie serwera
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
 });
