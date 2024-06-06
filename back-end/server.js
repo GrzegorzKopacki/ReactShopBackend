f18d827b8314ed31af3030f40ec9966e3d87fc2c
Grzegorz <gkopacki92@gmail.com>
2024-06-06 23:38:04 +0200

    .

--- back-end/server.js
+++ back-end/server.js
@@ -1,21 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-app.use(express.static('public'));
+// Serwowanie statycznych plików z folderu 'public'
+app.use(express.static("public"));
 
-app.get('/data', (req, res) => {
-    const dbPath = path.join(__dirname, 'db.json');
-    fs.readFile(dbPath, 'utf8', (err, data) => {
-        if (err) {
-            return res.status(500).send('Error reading database file.');
-        }
-        res.send(JSON.parse(data));
-    });
+// Endpoint dla ścieżki root
+app.get("/", (req, res) => {
+	res.send("Hello, World!");
 });
 
+// Endpoint do zwracania danych z pliku db.json
+app.get("/data", (req, res) => {
+	const dbPath = path.join(__dirname, "db.json");
+	fs.readFile(dbPath, "utf8", (err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading database file.");
+		}
+		res.send(JSON.parse(data));
+	});
+});
+
+// Uruchomienie serwera
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
